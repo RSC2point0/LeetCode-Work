4a72419950c54fc2d31ffec4c8895074b0ed9694
RSC2point0 <110577648+RSC2point0@users.noreply.github.com>
2022-08-20 12:06:49 -0400

    Time: 118 ms (51.77%), Space: 42.5 MB (96.88%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -10,11 +10,12 @@ var mySqrt = function(x) {
     //once you exit while you hit your target so you can return multiplier
     
     let j = 0;
-    
+    let currentProd = j * j;
     while(j * j <= x){
         //take each i and keep mult by itself until we reach or exceed x
         j++;
-
+        currentProd = j * j;
+  
     }
     return j - 1;
 };
