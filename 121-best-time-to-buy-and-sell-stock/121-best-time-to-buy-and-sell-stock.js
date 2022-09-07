2a75f65129d8a4432999047d3db0317a8fb01fd0
RSC2point0 <110577648+RSC2point0@users.noreply.github.com>
2022-09-06 20:44:39 -0400

    Time: 134 ms (41.45%), Space: 51.7 MB (67.44%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    //iterat thr prices
+        //subtract each buy $ from sell price and compare prof
+    //return max
+    let max = 0;
+    let min = prices[0];
+    for(let i = 0; i < prices.length; i++){
+        let sellPrice = prices[i];
+        max = Math.max(sellPrice - min, max);
+        //reacalc the lowest possible buy price
+        min = Math.min(prices[i], min);
+        
+    }
+    return max > 0 ? max : 0;
+};
