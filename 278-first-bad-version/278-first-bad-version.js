0875e9317a192a92ae49961a6d736ee60fd0bb49
RSC2point0 <110577648+RSC2point0@users.noreply.github.com>
2022-09-16 22:27:53 -0400

    Time: 65 ms (89.91%), Space: 42 MB (41.56%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -31,12 +31,13 @@ var solution = function(isBadVersion) {
         let left = 1;
         let right = n;
         while(left < right){
-            let mid = Math.floor((left + right)/2);
+            let mid = Math.floor((left + right)/2)
             if(isBadVersion(mid)){
                 right = mid;
             }else{
                 left = mid + 1;
             }
+           
         }
         return right;
     };
