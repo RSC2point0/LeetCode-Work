68970132d1f4be1fad643c62e478df3f2878096f
RSC2point0 <110577648+RSC2point0@users.noreply.github.com>
2022-09-28 21:54:49 -0400

    Time: 129 ms (30.45%), Space: 44.3 MB (57.85%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+
+      if(image[sr][sc] === color || image[sr][sc] === color || image === undefined){
+         return image;
+       }
+    const oldColor = image[sr][sc];
+    const changeArray = (image, r, c, oldColor, color) => {
+       //check if row or column are out of bounds toward top or out of bounds toward the bottom
+       if(r < 0 || r >= image.length || c < 0 || c >= image[0].length || image[r][c] !== oldColor) return;
+      
+        image[r][c] = color;
+        changeArray(image, r -1, c, oldColor, color);//top of curr pixel
+        changeArray(image, r + 1, c, oldColor, color);//bottom of curr pixel
+        changeArray(image, r, c - 1, oldColor, color);//left of curr pixel
+        changeArray(image, r, c + 1, oldColor, color);//right of curr pixel
+    }
+    //pass initial row and column coords
+    changeArray(image, sr, sc, oldColor,color);
+    
+    //return new image
+    return image;
+};
+
+   
+
+
+
+
